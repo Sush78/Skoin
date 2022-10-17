fb2a0af6e7164fda58ec484443edb9a9f1f206c3
Sushant Kumar <sushantkumar@Sushants-MacBook-Air-2.local>
2022-10-17 12:03:54 +0100

    added tests and completed Token functions

--- smart_contract/contracts/SkoinToken.sol
+++ smart_contract/contracts/SkoinToken.sol
@@ -6,6 +6,7 @@ contract SkoinToken {
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
     string public name = "Skoin";    
     string public symbol = "SKM";    
     string public standard = "Skoin Toekn 1.0";    
@@ -14,6 +15,12 @@ contract SkoinToken {
         address indexed _from,
         address indexed _to,
         uint256 _value 
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
     );
 
     constructor(uint256 _initialSupply){
@@ -28,5 +35,24 @@ contract SkoinToken {
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Account to approve to send Skoin tokens on my behalf
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Transfer Skoin tokens to approved account, msg.sender is a thrid party and _from will be the owner
+    function transferFrom(address payable _from, address payable _to, uint256 _value) public returns(bool success){
+        require(balanceOf[_from] >= _value, "Balance is less than transfer value");
+        require(allowance[_from][msg.sender] >= _value, "allowance is less than the transfer value");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
     }
 }
--- smart_contract/test/SkoinToken.js
+++ smart_contract/test/SkoinToken.js
@@ -13,9 +13,9 @@ describe("SkoinToken", () =>{
         const SkoinToken = await ethers.getContractFactory("SkoinToken");
         const skoinToken = await SkoinToken.deploy(_initialSupply);
 
-        const [owner, otherAccount] = await ethers.getSigners();
+        const [owner, otherAccount, otherAccount2, otherAccount3] = await ethers.getSigners();
 
-        return { skoinToken, totalSupply, owner, otherAccount };
+        return { skoinToken, totalSupply, owner, otherAccount, otherAccount2, otherAccount3 };
     }
 
     describe("Deployment", function () {
@@ -45,10 +45,46 @@ describe("SkoinToken", () =>{
             "Balance is less than amount"
           );
         });
-        it("Should set the right standard", async function () {
+        it("Should subtract the right amount from senders balance", async function () {
           const { skoinToken, totalSupply ,owner, otherAccount } = await loadFixture(deployToken);
-          await skoinToken.transfer(otherAccount.address, 250);
-          expect(await skoinToken.balanceOf(owner.address)).to.equal(totalSupply-250);
+          expect(await skoinToken.transfer(otherAccount.address, 250))
+            .to.changeTokenBalance(skoinToken, owner.address, totalSupply+250);
+        });
+        it("Should set add the right amount to receivers balance", async function () {
+          const { skoinToken , otherAccount } = await loadFixture(deployToken);
+          expect(await skoinToken.transfer(otherAccount.address, 250))
+            .to.changeTokenBalance(skoinToken, otherAccount.address, 250);
+        });
+        it("Should approve tokens for delegated transfer", async function () {
+          const { skoinToken, totalSupply ,owner, otherAccount } = await loadFixture(deployToken);
+          expect(await skoinToken.approve(otherAccount.address, 100)).to.emit(skoinToken, "Approval")
+            .withArgs(owner.address, otherAccount.address , 100);;
+        });
+        it("Should change the allowance correctly", async function () {
+          const { skoinToken, owner, otherAccount } = await loadFixture(deployToken);
+          await skoinToken.approve(otherAccount.address, 100);
+          const returnAllowance = await skoinToken.allowance(owner.address, otherAccount.address);
+          expect(returnAllowance.toNumber()).to.equal(100);
+        });
+        it("Should check value is more than transfer amount for transferFrom", async function () {
+          const { skoinToken, otherAccount, otherAccount2, otherAccount3 } = await loadFixture(deployToken);
+          await expect(skoinToken.transferFrom(otherAccount.address, otherAccount2.address, 102)).to.be.revertedWith(
+            "Balance is less than transfer value"
+          );
+        });
+        it("Should check allowance value is more than transfer amount for transferFrom", async function () {
+          const { skoinToken, owner ,otherAccount } = await loadFixture(deployToken);
+          await skoinToken.approve(otherAccount.address, 101);
+          await expect(skoinToken.transferFrom(owner.address, otherAccount.address, 102)).to.be.revertedWith(
+            "allowance is less than the transfer value"
+          );
+        });
+        it("Should upadte balance correctly for transferFrom", async function () {
+          const { skoinToken, owner, otherAccount ,otherAccount2, totalSupply } = await loadFixture(deployToken);
+          await skoinToken.approve(otherAccount.address, 102);
+          await skoinToken.allowance(owner.address,otherAccount.address);
+          await skoinToken.connect(otherAccount).transferFrom(owner.address, otherAccount.address, 100);
+          expect(await skoinToken.balanceOf(owner.address)).to.be.equal(totalSupply-100);
         });
     })
 })
